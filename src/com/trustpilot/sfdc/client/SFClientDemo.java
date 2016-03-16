509e6d1eb99389a3d2f14b96494382796917b761
Amandeep Midha <amandeepm@gmx.com>
2016-03-16 02:35:24 +0100

    Added library compiled from https://github.com/forcedotcom/wsc

---
+++ src/com/trustpilot/sfdc/client/SFClientDemo.java
@@ -0,0 +1,12 @@
+package com.trustpilot.sfdc.client;
+
+import com.sforce.soap.enterprise.Connector;
+import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.soap.enterprise.QueryResult;
+import com.sforce.soap.enterprise.sobject.Contact;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+
+public class SFClientDemo {
+
+}
