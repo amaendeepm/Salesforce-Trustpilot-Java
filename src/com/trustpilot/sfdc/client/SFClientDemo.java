b9b9aa86fbc4eade743809d722646ff5f0709ef7
Amandeep Midha <amandeepm@gmx.com>
2016-03-16 02:50:34 +0100

    corrected username

--- src/com/trustpilot/sfdc/client/SFClientDemo.java
+++ src/com/trustpilot/sfdc/client/SFClientDemo.java
@@ -8,5 +8,40 @@ import com.sforce.ws.ConnectionException;
 import com.sforce.ws.ConnectorConfig;
 
 public class SFClientDemo {
+	private static final String USERID = "ami@trustpilot.com.tpreviews";
+	private static final String PASSWORD = "Charmie12345!";
+	private static final String SECURITYTOKEN = "kCdHLCa9SCI7x8AVvEhNQhez";
 
+	public static void main(String[] args) {
+		try {
+			ConnectorConfig connectorConfig = new ConnectorConfig();
+			connectorConfig.setUsername(USERID);
+			connectorConfig.setPassword(PASSWORD + SECURITYTOKEN);
+			connectorConfig.setPrettyPrintXml(true);
+			connectorConfig.setTraceMessage(true);
+			EnterpriseConnection connection = Connector
+					.newConnection(connectorConfig);
+			queryContacts(connection);
+		} catch (ConnectionException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void queryContacts(EnterpriseConnection connection) {
+		try {
+			QueryResult queryResults = connection
+					.query("SELECT Id, FirstName, LastName " + "FROM Contact ");
+			if (queryResults.getSize() > 0) {
+				for (int i = 0; i < queryResults.getRecords().length; i++) {
+					com.sforce.soap.enterprise.sobject.Contact c = (Contact) queryResults
+							.getRecords()[i];
+					System.out.println(c.getFirstName() + c.getLastName()
+							+ c.getId());
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
